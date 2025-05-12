474fc0773b69c69d9a1b94ab375986ed7eb661fa
Timberfang <timberfang.code@pm.me>
2025-05-12 10:07:20 -0500

    chore: Clean up formatting

--- LicenseIt/Program.cs
+++ LicenseIt/Program.cs
@@ -7,28 +7,18 @@ internal static class Program
 	private static async Task<int> Main(string[] args)
 	{
 		RootCommand rootCommand = new("License creator.");
-		Option<string> authorOption = new("--author-name")
-		{
-			IsRequired = true,
-			Description = "Name(s) of the author(s). If multiple names are needed, separate them by commas."
-		};
-		Option<string> projectOption = new("--project-name")
-		{
-			IsRequired = true, Description = "Name of the project the license will be applied to."
-		};
-		Option<int> yearOption = new("--year") { Description = "Year when development began on the project." };
-		Option<string> emailOption = new("--email-address") { Description = "Email address(es) of the author(s)." };
-		Option<string> licenseOption = new("--license-name")
-		{
-			IsRequired = true,
-			Description =
-				"Name of the license (e.g. MIT). To view all valid license names, use the 'list' command."
-		};
-		Option<string> outputOption = new("--output")
-		{
-			Description =
-				"Path to the output file. Defaults to ${working-directory} -> ${project-name} -> LICENSE."
-		};
+		Option<string> authorOption = new("--author-name",
+			"Name(s) of the author(s). If multiple names are needed, separate them by commas.") { IsRequired = true };
+		Option<string> projectOption = new("--project-name",
+			"Name of the project the license will be applied to.") { IsRequired = true };
+		Option<string> licenseOption = new("--license-name",
+			"Name of the license (e.g. MIT). Use the 'list' command to view all licenses.") { IsRequired = true };
+		Option<int> yearOption = new("--year",
+			"Year when development began on the project.");
+		Option<string> emailOption = new("--email-address",
+			"Email address(es) of the author(s).");
+		Option<string> outputOption = new("--output",
+			"Path to the output file. Defaults to ${working-directory} -> ${project-name} -> LICENSE.");
 		yearOption.SetDefaultValue(DateTime.Now.Year);
 		authorOption.AddAlias("--author");
 		authorOption.AddAlias("-a");
